841529d3d5fbdfdde82c9651bb0ecb4350d4a104
Jiří Prokop <jiri@bayzat.com>
2024-03-16 23:01:01 +0100

    Add PoC of intro animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import { Canvas, MaterialNode, ThreeEvent, extend } from '@react-three/fiber'
 import { times } from 'lodash';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Bloom, ChromaticAberration, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing';
 import { Environment } from '@react-three/drei';
-import { MeshStandardMaterial, WebGLProgramParametersWithUniforms } from 'three';
-
-type LaserProps = {
-  color: 'red' | 'blue'
-}
-
-function Laser({ color }: LaserProps) {
-  return (
-    <mesh position={[0, 0, 2]}>
-      <boxGeometry args={[0.005, 0.005, 4]} />
-      <meshStandardMaterial emissive={color === 'red' ? 'hotpink' : 'skyblue'} emissiveIntensity={12} />
-    </mesh>
-  );
-}
+import { Mesh, MeshStandardMaterial, Vector4, WebGLProgramParametersWithUniforms } from 'three';
+import gsap from 'gsap';
 
 class CustomMaterial extends MeshStandardMaterial {
+  uEdges!: Vector4;
+
   onBeforeCompile(parameters: WebGLProgramParametersWithUniforms) {
+    parameters.uniforms.uEdges = { value: this.uEdges };
+
     parameters.vertexShader = parameters.vertexShader
       .replace(
         '#define STANDARD',
@@ -33,7 +25,7 @@ class CustomMaterial extends MeshStandardMaterial {
     parameters.fragmentShader = parameters.fragmentShader
       .replace(
         '#define STANDARD',
-        '#define STANDARD\nvarying vec2 vMyUv;\nvarying vec3 vMyNormal;'
+        '#define STANDARD\nvarying vec2 vMyUv;\nvarying vec3 vMyNormal;\nuniform vec4 uEdges;'
       )
       .replace(
         'vec4 diffuseColor = vec4( diffuse, opacity );',
@@ -44,10 +36,20 @@ class CustomMaterial extends MeshStandardMaterial {
             float edgeWidth = 0.03;
             float edgeTransition = 0.01;
             float edgeStart = edgeWidth - edgeTransition;
-            float edgeBlend = smoothstep(edgeStart, edgeWidth, vMyUv.x);
-            edgeBlend *= smoothstep(edgeStart, edgeWidth, vMyUv.y);
-            edgeBlend *= smoothstep(edgeStart, edgeWidth, 1.0 - vMyUv.x);
-            edgeBlend *= smoothstep(edgeStart, edgeWidth, 1.0 - vMyUv.y);
+            float edgeBlend = 1.0;
+
+            if (uEdges.x == 1.0) {
+              edgeBlend *= smoothstep(edgeStart, edgeWidth, vMyUv.x);
+            }
+            if (uEdges.y == 1.0) {
+              edgeBlend *= smoothstep(edgeStart, edgeWidth, vMyUv.y);
+            }
+            if (uEdges.z == 1.0) {
+              edgeBlend *= smoothstep(edgeStart, edgeWidth, 1.0 - vMyUv.x);
+            }
+            if (uEdges.w == 1.0) {
+              edgeBlend *= smoothstep(edgeStart, edgeWidth, 1.0 - vMyUv.y);
+            }
 
             vec3 edgeColor = diffuseColor.rgb * 0.125;
             diffuseColor.rgb = mix(diffuseColor.rgb, edgeColor, 1.0 - edgeBlend);
@@ -67,9 +69,50 @@ declare module '@react-three/fiber' {
   }
 }
 
+type LaserProps = {
+  color: 'red' | 'blue'
+}
+
+function Laser({ color }: LaserProps) {
+  return (
+    <mesh position={[0, 0, 2]}>
+      <boxGeometry args={[0.005, 0.005, 4]} />
+      <meshStandardMaterial emissive={color === 'red' ? 'hotpink' : 'skyblue'} emissiveIntensity={12} />
+    </mesh>
+  );
+}
+
+type FieldProps = {
+  x: number;
+  y: number;
+  color: string;
+  onClick: (event: ThreeEvent<MouseEvent>, x: number, y: number) => void;
+}
+
+function Field({ x, y, color, onClick }: FieldProps) {
+  const meshRef = useRef<Mesh>(null!);
+
+  useEffect(() => {
+    gsap.to(meshRef.current.position, {
+      x: x - 3,
+      y: y - 3,
+      z: -5,
+      duration: 1 + Math.min(x, y) / 10,
+      ease: "power4.inOut",
+    });
+  }, [x, y]);
+
+  return <mesh ref={meshRef} position={[x - 3, y - 3, -15]} onClick={(event) => onClick(event, x, y)}>
+    <boxGeometry args={[1, 1, 10]} />
+    <customMaterial
+      color={color}
+      uEdges={new Vector4(x === 0 ? 0 : 1, y === 0 ? 0 : 1, x === 6 ? 0 : 1, y === 6 ? 0 : 1)}
+    />
+  </mesh>
+}
+
 function App() {
   const [colors, setColors] = useState(new Map<string, string>());
-
   const [isX, setIsX] = useState(false);
 
   function handleClick(event: ThreeEvent<MouseEvent>, x: number, y: number) {
@@ -91,10 +134,13 @@ function App() {
         <directionalLight color="white" intensity={3} position={[10, 0, 100]} />
         {times(7, (x: number) => (
           times(7, (y: number) => (
-            <mesh key={`${x}:${y}`} position={[x - 3, y - 3, -5]} onClick={(event) => handleClick(event, x, y)}>
-              <boxGeometry args={[1, 1, 10]} />
-              <customMaterial color={colors.get(`${x}:${y}`) ?? "gray"} />
-            </mesh>
+            <Field
+              key={`${x}:${y}`}
+              x={x}
+              y={y}
+              color={colors.get(`${x}:${y}`) ?? "gray"}
+              onClick={handleClick}
+            />
           ))
         ))}
         {false && <Laser color="red" />}
