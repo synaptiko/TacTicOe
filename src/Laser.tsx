63a1912858e511f666262b8dc2845af6bb03e104
Jiří Prokop <jiri@bayzat.com>
2024-03-21 23:24:16 +0100

    Add laser animation for X symbol; improve laser's appearance, add hot-spot

--- src/Laser.tsx
+++ src/Laser.tsx
@@ -1,3 +1,5 @@
+import { Color } from 'three';
+import { symbolThickness } from './consts';
 import { Player } from './types';
 
 type LaserProps = {
@@ -6,14 +8,29 @@ type LaserProps = {
   player: Player;
 };
 
+function mix(color1: string, color2: string, factor: number) {
+  return new Color(color1).lerp(new Color(color2), factor);
+}
+
 export function Laser({ x, y, player }: LaserProps) {
+  {
+    /* TODO: move these colors into consts file too */
+  }
+  const laserColor = player === 'x' ? '#E72929' : '#299CE7';
+  const hotSpotColor = mix(laserColor, 'white', 0.25);
+
   return (
-    <mesh position={[x - 3, y - 3, 4]}>
-      {/* TODO: use plane that always face the camera instead of box to save some "resources" */}
-      <boxGeometry args={[0.005, 0.005, 8]} />
-      {/* TODO: add some slight shimmering effect with shader to sell the laser effect more */}
-      {/* TODO: move these colors into consts file too */}
-      <meshStandardMaterial emissive={player === 'x' ? '#E72929' : '#299CE7'} emissiveIntensity={12} />
-    </mesh>
+    <>
+      <mesh position={[x - 3, y - 3, 4]}>
+        {/* TODO: use plane that always face the camera instead of box to save some "resources" */}
+        <boxGeometry args={[0.005, 0.005, 8]} />
+        {/* TODO: add some slight shimmering effect with shader to sell the laser effect more */}
+        <meshStandardMaterial emissive={laserColor} emissiveIntensity={12} />
+      </mesh>
+      <mesh position={[x - 3, y - 3, 0.01]}>
+        <planeGeometry args={[symbolThickness, symbolThickness]} />
+        <meshStandardMaterial emissive={hotSpotColor} emissiveIntensity={5} />
+      </mesh>
+    </>
   );
 }
--- src/Lasers.tsx
+++ src/Lasers.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from 'react';
 import { Laser } from './Laser';
-import { Group, Object3D } from 'three';
+import { Group, Object3D, Vector2 } from 'three';
 import gsap from 'gsap';
-import { drawingDuration, symbolGap, symbolRadius } from './consts';
+import { drawingDuration, symbolGap, symbolRadius, xSymbolScale } from './consts';
 import { Player } from './types';
 
 type LasersProps = {
@@ -26,16 +26,64 @@ class LaserOAnimation {
 }
 
 class LaserXAnimation {
+  private path: Vector2[] = [];
+  private segmentLengths: number[] = [];
+  private totalPathLength: number = 0;
+
   constructor(
     private laser: Object3D,
-    private amplitude: number,
-    private offset: number = 0
-  ) {}
+    private w: number,
+    private h: number,
+    private flip: boolean = false
+  ) {
+    this.calculatePath();
+  }
 
-  set value(angle: number) {
-    const { laser, amplitude, offset } = this;
+  private calculatePath() {
+    const wh = this.w * 0.5;
+    const angleRad = (-45 * Math.PI) / 180;
+    const basePoints = [
+      new Vector2(-wh, wh + this.h),
+      new Vector2(wh, wh + this.h),
+      new Vector2(wh, wh),
+      new Vector2(wh + this.h, wh),
+      new Vector2(wh + this.h, -wh),
+      new Vector2(wh, -wh),
+      new Vector2(wh, -wh - this.h),
+    ].map(
+      (point) =>
+        new Vector2(
+          point.x * Math.cos(angleRad) - point.y * Math.sin(angleRad),
+          point.x * Math.sin(angleRad) + point.y * Math.cos(angleRad)
+        )
+    );
 
-    laser.position.set(amplitude * Math.cos(angle - offset), amplitude * Math.sin(angle - offset), 0);
+    this.path = this.flip ? basePoints.map((p) => new Vector2(-p.x, -p.y)) : basePoints;
+
+    this.segmentLengths = [];
+    this.totalPathLength = 0;
+
+    for (let i = 1; i < this.path.length; i++) {
+      const segmentLength = this.path[i].distanceTo(this.path[i - 1]);
+      this.segmentLengths.push(segmentLength);
+      this.totalPathLength += segmentLength;
+    }
+  }
+
+  set value(progress: number) {
+    let cumulativeLength = 0;
+
+    for (let i = 1; i < this.path.length; i++) {
+      if ((cumulativeLength + this.segmentLengths[i - 1]) / this.totalPathLength > progress) {
+        const segmentFraction = (progress * this.totalPathLength - cumulativeLength) / this.segmentLengths[i - 1];
+        const currentPoint = new Vector2().lerpVectors(this.path[i - 1], this.path[i], segmentFraction);
+        this.laser.position.set(currentPoint.x, currentPoint.y, 0);
+
+        break;
+      }
+
+      cumulativeLength += this.segmentLengths[i - 1];
+    }
   }
 }
 
@@ -47,10 +95,10 @@ export function Lasers({ x, y, player }: LasersProps) {
     const animations: gsap.TweenTarget[] = [];
 
     if (player === 'x') {
-      animations.push(
-        new LaserXAnimation(laser1Ref.current, symbolRadius),
-        new LaserXAnimation(laser2Ref.current, symbolRadius - symbolGap, Math.PI)
-      );
+      const w = symbolGap;
+      const h = (symbolRadius - symbolGap) * xSymbolScale;
+
+      animations.push(new LaserXAnimation(laser1Ref.current, w, h), new LaserXAnimation(laser2Ref.current, w, h, true));
     } else {
       animations.push(
         new LaserOAnimation(laser1Ref.current, symbolRadius),
@@ -67,8 +115,8 @@ export function Lasers({ x, y, player }: LasersProps) {
         value: 0,
       },
       {
-        value: 2 * Math.PI,
-        runBackwards: true,
+        value: player === 'x' ? 1 : 2 * Math.PI,
+        runBackwards: player === 'o',
         duration: drawingDuration,
         ease: 'none',
         onComplete: () => {
